729cdb6f5d8e007f84e156e83f497f53663d9c66
0xflotus <0xflotus@gmail.com>
2026-09-20 01:11:15 +0200

    feat: add benchmark tests (#45)

---
+++ vitest.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    include: ["**/*.test.js"],
+    benchmark: {
+      include: ["**/*.bench.js"],
+      suppressExportGetterWarnings: true,
+    },
+  },
+});
